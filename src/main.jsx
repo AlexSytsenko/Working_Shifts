52af7f04a44ff7e0dff52ecc0d084045973c5e54
Alex <68234661+AlexSytsenko@users.noreply.github.com>
2026-05-11 16:23:53 +0300

    fixed analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  initAnalytics,
+  trackPageView,
+} from "./utils/analytics";
+  
+initAnalytics();
+trackPageView();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
